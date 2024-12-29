6c0ee8d84945955ad645434d8f651fb051c92922
shehryar-munir <shehryar.munir@dubizzlelabs.com>
2024-12-29 23:53:06 +0500

    Added images and content of first two projects.

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,46 +2,61 @@ import React from "react";
 import type {Metadata} from "next";
 import {projects} from "./project-data";
 import Image from "next/image";
+import Jarvis from "./Jarvis/Jarvis";
+import EEG from "./EEG/EEG";
 
 export const metadata: Metadata = {
     title: "Projects",
     description: "My Projects",
 };
 export default function Projects() {
+    console.log("(projects?.[0]: ",projects?.[0]);
     return (
         <section>
-            <h1 className="mb-8 text-2xl font-medium tracking-tight">Projects</h1>
+            {/*<h1 className="mb-8 text-2xl font-medium tracking-tight">Projects</h1>*/}
             <div className="space-y-6">
-                {projects.map((project, index) => (
-                    <a
-                        key={index}
-                        href={project?.githubUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="block group hover:opacity-80 transition-opacity duration-200"
-                    >
-                        <div className="flex flex-col sm:flex-row sm:justify-between">
-                            <div className="flex flex-col sm:flex-row sm:items-center">
-                                <span className="text-black dark:text-white font-medium tracking-tight">
-                                    {project.title}
-                                </span>
-                                <Image
-                                    src={project?.imgUrl}
-                                    alt="Logo"
-                                    height={project?.height}
-                                    width={project?.width}
-                                    className="mt-2 sm:ml-4 sm:mt-0"
-                                />
-                            </div>
-                            <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm sm:mt-0 mt-2">
-                                {project.year}
-                            </span>
-                        </div>
-                        <p className="prose prose-neutral dark:prose-invert pt-3">
-                            {project.description}
-                        </p>
-                    </a>
-                ))}
+                <Jarvis />
+                <EEG />
+                {/*{projects.map((project, index) => (*/}
+                {/*    <a*/}
+                {/*        key={index}*/}
+                {/*        href={project?.githubUrl}*/}
+                {/*        target="_blank"*/}
+                {/*        rel="noopener noreferrer"*/}
+                {/*        className="block group hover:opacity-80 transition-opacity duration-200"*/}
+                {/*    >*/}
+                {/*        <div className="flex flex-col sm:flex-row sm:justify-between">*/}
+                {/*            <div className="flex flex-col sm:flex-row sm:items-center">*/}
+                {/*                <span className="text-black dark:text-white font-medium tracking-tight">*/}
+                {/*                    {project.title}*/}
+                {/*                </span>*/}
+                {/*                <Image*/}
+                {/*                    src={project?.imgUrl}*/}
+                {/*                    alt="Logo"*/}
+                {/*                    height={project?.height}*/}
+                {/*                    width={project?.width}*/}
+                {/*                    className="mt-2 sm:ml-4 sm:mt-0"*/}
+                {/*                />*/}
+                {/*            </div>*/}
+                {/*            <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm sm:mt-0 mt-2">*/}
+                {/*                {project.year}*/}
+                {/*            </span>*/}
+                {/*        </div>*/}
+                {/*        {!!project?.imgArray && project?.imgArray.length > 0 &&*/}
+                {/*            <div className="flex flex-col sm:flex-row sm:justify-between flex-wrap border-2">*/}
+                {/*                {project?.imgArray.map((img, index) => {*/}
+                {/*                    return <img key={index} src={img} className={"h-2/5 w-2/5 m-3 border-2"}  alt="Logo"/>*/}
+                {/*                })*/}
+                {/*                }*/}
+                {/*            </div>*/}
+                {/*        }*/}
+
+
+                {/*        <p className="prose prose-neutral dark:prose-invert pt-3">*/}
+                {/*            {project.description}*/}
+                {/*        </p>*/}
+                {/*    </a>*/}
+                {/*))}*/}
             </div>
         </section>
     );
--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -6,6 +6,7 @@ export interface Project {
     githubUrl: string;
     height: number;
     width: number;
+    imgArray: Array<string>;
 }
 
 export const projects: Project[] = [
@@ -20,7 +21,9 @@ export const projects: Project[] = [
         imgUrl: "/jarvis_logo.png",
         height: 50,
         width: 50,
-        githubUrl: ""
+        githubUrl: "",
+        imgArray: [  "jarvis_crm/crm_2.png", "jarvis_crm/crm_1.png","jarvis_crm/crm_3.png", "jarvis_crm/crm_4.png", "jarvis_crm/crm_7.png", "jarvis_crm/crm_8.png" ],
+
     },
     {
         title: "Epileptic Seizure Prediction using EEG – Final Year Project",
@@ -32,7 +35,9 @@ export const projects: Project[] = [
         imgUrl: "/eeg_logo.png",
         height: 40,
         width: 40,
-        githubUrl: "https://github.com/burakbinmunir/Epileptic-Seizure-Prediction-Using-EEG-Recordings"
+        githubUrl: "https://github.com/burakbinmunir/Epileptic-Seizure-Prediction-Using-EEG-Recordings",
+        imgArray: ["eeg_prediction/eeg_1.jpg", "eeg_prediction/eeg_2.png", "eeg_prediction/eeg_3.png", "eeg_prediction/eeg_4.png", "eeg_prediction/eeg_5.png" ],
+
     },
     {
         title: "Invictus – Web Engineering",
@@ -43,7 +48,8 @@ export const projects: Project[] = [
         imgUrl: "/invictus_logo.png",
         height: 150,
         width: 150,
-        githubUrl: "https://github.com/dark-coder12/Invictus"
+        githubUrl: "https://github.com/dark-coder12/Invictus",
+        imgArray: [ "invictus/invictus_1.png", "invictus/invictus_2.png", "invictus/invictus_3.png", "invictus/invictus_4.png", "invictus/invictus_5.png", "invictus/invictus_6.png" ],
     },
     {
         title: "Multiclass Brain Tumor Classification using MRI – Artificial Intelligence",
@@ -54,7 +60,8 @@ export const projects: Project[] = [
         imgUrl: "/brain_tumor.png",
         height: 40,
         width: 40,
-        githubUrl: "https://github.com/dark-coder12/multiclass-brain-tumor-classification-using-mri"
+        githubUrl: "https://github.com/dark-coder12/multiclass-brain-tumor-classification-using-mri",
+        imgArray: ["chest_xray_classification/chest_1.png","chest_xray_classification/chest_2.png", "chest_xray_classification/chest_3.png", "chest_xray_classification/chest_4.png"]
     },
     {
         title: "File-Crypt – Information Security",
@@ -65,7 +72,8 @@ export const projects: Project[] = [
         imgUrl: "/file_crypt_logo.png",
         height: 100,
         width: 100,
-        githubUrl: "https://github.com/burakbinmunir/File-Encryption"
+        githubUrl: "https://github.com/burakbinmunir/File-Encryption",
+        imgArray: ["file_crypt/file_1.png", "file_crypt/file_2.png", "file_crypt/file_3.png","file_crypt/file_4.png"],
     },
 
 ];
